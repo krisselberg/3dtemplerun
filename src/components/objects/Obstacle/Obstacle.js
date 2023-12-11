d8434170dd9a0a427256ff482eb26314389094c5
Kristoffer Selberg <kselberg@dynamic-oit-vapornet100-10-8-45-212.princeton.edu>
2023-12-11 15:16:58 -0500

    updated obstacle with random cube / pyramid

--- src/components/objects/Obstacle/Obstacle.js
+++ src/components/objects/Obstacle/Obstacle.js
@@ -1,16 +1,40 @@
-import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three';
+import { BoxGeometry, CylinderGeometry, Mesh, MeshBasicMaterial } from 'three';
 
 class Obstacle extends Mesh {
-    constructor(size = 50, color = 0xff0000) {
-        // Create the geometry and material for the cube
-        const geometry = new BoxGeometry(size, size, size);
+    constructor() {
+        // Randomly choose the type of obstacle - cube or tree stump
+        const isCube = Math.random() > 0.5;
+
+        // Cube geometry and size
+        const cubeSize = 1;
+        const cubeGeometry = new BoxGeometry(cubeSize, cubeSize, cubeSize);
+
+        // Tree stump geometry and size
+        const stumpRadius = 4;
+        const stumpHeight = 6;
+        const stumpGeometry = new CylinderGeometry(
+            stumpRadius,
+            stumpRadius,
+            stumpHeight,
+            4
+        );
+
+        // Select geometry based on random choice
+        const geometry = isCube ? cubeGeometry : stumpGeometry;
+
+        // Random color for the obstacle
+        const color = Math.random() * 0xffffff;
         const material = new MeshBasicMaterial({ color });
 
         // Call the Mesh constructor
         super(geometry, material);
 
-        // Set the initial position of the cube
-        this.position.set(0, size / 2, 0); // Adjust as needed
+        // Set the initial position of the obstacle
+        if (isCube) {
+            this.position.set(0, 0, 0); // For cube
+        } else {
+            this.position.set(0, 0, 0); // For tree stump
+        }
     }
 }
 
